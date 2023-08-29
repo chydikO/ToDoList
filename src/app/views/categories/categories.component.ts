1283366819fd2a28730cabf12b4dcbd43d20e228
Chudnovskiy.O.W520 <oleg.chudnovskiy@gmail.com>
2023-08-29 18:19:05 +0300

    implemented obtaining categories and the entire list of tasks in the style of reactive programmingÐ

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -20,7 +20,8 @@ export class CategoriesComponent implements OnInit {
 
     // метод вызывается автоматически после инициализации компонента
     ngOnInit() {
-        this.categories = this.dataHandler.getCategories();
+        // this.categories = this.dataHandler.getCategories();
+      this.dataHandler.categoriesSubject.subscribe(categories => this.categories = categories);
         console.log(this.categories);
     }
 
